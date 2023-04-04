a49a8aef767ec31cca9bd1dcee504d370c897354
mdanilenco <mihail.danilenco18@gmail.com>
2023-04-04 20:40:45 +0300

    Last changes

--- LAB1/prototype/PoliceCarManager.java
+++ LAB1/prototype/PoliceCarManager.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class PoliceCarManager {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String choice;
